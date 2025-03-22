111efcfc7db1691e290d3735f93c59b878966a0e
ManognaBandhu <157211161+ManognaBandhu@users.noreply.github.com>
2025-03-22 10:04:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Fighting the darkness/fighting-the-darkness.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+// User function Template for Java
+
+class Solution {
+    long maxDays(int arr[]) {
+        // code here
+        //the max no of days the room will be illuminated
+        long maxDay = 0;
+        //find the max candle size (which determines the no of days)
+        for(int candle : arr){
+            maxDay = Math.max(maxDay, candle);
+        }
+        return maxDay;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            String input = br.readLine();
+            String[] numbers = input.split("\\s+");
+            int[] arr = new int[numbers.length];
+
+            for (int i = 0; i < numbers.length; i++) {
+                arr[i] = Integer.parseInt(numbers[i]);
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.maxDays(arr));
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
